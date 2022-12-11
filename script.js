b2ef50daae2654152ccb3b27458b5f5c7a99b81b
MAIKA <maikanguyenn@gmail.com>
2022-12-11 22:42:48 +0100

    added js onclick function

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-   function myFunction() {
+
+function myFunction() {
         var x = document.getElementById("activiteit");
-        if (x.style.display === "none") {
+        if (x.style.display !== "none") {
           x.style.display = "block";
         } else {
           x.style.display = "none";
         }
-      }
+
+       /* var button = document.getElementById("btn").value;*/
+        document.getElementById("btn").innerHTML = '<i class="fa fa-trash" style=" font-size:1.2rem; color: white; margin-right: 5px"></i> Zet uit mijn leeslijst';
+      };
+
+   
+   /* old function
+
+     function myFunction() {
+        var x = document.getElementById('activiteit');
+        if (x.style.display !== 'none') {
+          x.style.display = 'none';
+        } 
+        else {
+          x.style.display = 'block';
+        }
+      }; */
+
+
