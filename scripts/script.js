7dd746481495e74144d92b434c3c6a1ab12bee19
MAIKA <maikanguyenn@gmail.com>
2023-01-24 00:24:45 +0100

    add feed forward leeslijst javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,18 @@
 
 function myFunction() {
-        var x = document.getElementById("activiteit");
-        if (x.style.display !== "none") {
-          x.style.display = "block";
-        } else {
-          x.style.display = "none";
-        }
+  var x = document.getElementById("activiteit");
+  if (x.style.display !== "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 
-       /* var button = document.getElementById("btn").value;*/
-        document.getElementById("btn").innerHTML = '<i class="fa fa-trash" style=" font-size:1.2rem; color: white; margin-right: 5px"></i> Zet uit mijn leeslijst';
-      };
+ document.getElementById("aantal").innerHTML +=
+  " (1)";
+  document.getElementById("btn").innerHTML = '<i class="fa fa-trash" style=" font-size:1.2rem; color: white; margin-right: 5px"></i> Zet uit mijn leeslijst';
+};
+
+
 
    
    /* old function
